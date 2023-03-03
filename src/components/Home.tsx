93742bc23b79a4629045f5a198bd14b172b95f4b
Ibranista <ibranista88@gmail.com>
2023-03-04 00:37:44 +0100

    style header text.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,20 @@ import SongList from "../styles/SongStyles";
 function Home() {
   return (
     <>
-      <h1>list of songs</h1>
+      <h1
+        className="
+        text-xl
+        text-left
+        font-bold
+        text-gray-800
+        mt-10
+        mb-10
+        md:text-6xl
+        md:text-center
+      "
+      >
+        Explore songs below
+      </h1>
       <SongList />
     </>
   );
