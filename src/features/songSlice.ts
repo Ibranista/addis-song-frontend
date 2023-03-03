8c3ebcb40cbe33575fdadccc35490561c8f60bde
Ibranista <ibranista88@gmail.com>
2023-03-03 12:18:32 +0100

    fix types.

--- src/features/songSlice.ts
+++ src/features/songSlice.ts
@@ -1,31 +1,47 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface Song {
+  id: number;
+  title: string;
+  artist: string;
+}
+
+interface SongState {
+  songs: Song[];
+  isLoading: boolean;
+}
+
+const initialState: SongState = {
+  songs: [],
+  isLoading: false,
+};
 
 export const songSlice = createSlice({
   name: "songs",
-  initialState: {
-    songs: [],
-    isLoading: false,
-  },
+  initialState,
   reducers: {
     getSongsFetch: (state) => {
       state.isLoading = true;
     },
-    getSongsSuccess: (state, action) => {
+    getSongsSuccess: (state, action: PayloadAction<Song[]>) => {
       state.songs = action.payload;
       state.isLoading = false;
     },
     getSongsFailure: (state) => {
       state.isLoading = false;
     },
-    addSong: (state, action) => {
+    addSong: (state, action: PayloadAction<Song>) => {
       state.songs.push(action.payload);
       console.log("add song: ", action.payload);
       console.log(state.songs);
     },
-    removeOneSong: (state, action) => {
-      state.songs;
+    removeOneSong: (state, action: PayloadAction<number>) => {
+      state.songs = state.songs.filter((song) => song.id !== action.payload);
     },
-    editSong: (state, action) => {
+    editSong: (
+      state,
+      action: PayloadAction<{ id: number; data: Partial<Song> }>
+    ) => {
       const { id, data } = action.payload;
       const songIndex = state.songs.findIndex((song) => song.id === id);
       if (songIndex >= 0) {
