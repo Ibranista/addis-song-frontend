ae6aaa0edd7634def6351dafe4f22b365950ced5
Ibranista <ibranista88@gmail.com>
2023-03-03 23:44:00 +0100

    change to localhost.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://addis-song-backend.onrender.com/",
+        // target: "https://addis-song-backend.onrender.com/",
+        target: "http://localhost:8000",
         changeOrigin: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
