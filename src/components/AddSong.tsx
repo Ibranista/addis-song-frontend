6998ed3132e6be9d40cc7a3656181be841c28076
Ibranista <ibranista88@gmail.com>
2023-03-04 00:49:24 +0100

    style add song form.

--- src/components/AddSong.tsx
+++ src/components/AddSong.tsx
@@ -29,8 +29,29 @@ function AddSong() {
   };
   return (
     <>
-      <main>
-        <form action="" onSubmit={onSubmit}>
+      <main className="bg-slate-300">
+        <h1
+          className="
+        text-xl
+        text-left
+        font-bold
+        text-gray-800
+        mt-10
+        mb-10
+        md:text-6xl
+        md:text-center
+        "
+        >
+          Add Song Form
+        </h1>
+        <form
+          action=""
+          onSubmit={onSubmit}
+          className="flex flex-col items-center justify-center p-10
+          bg-gradient-to-r from-gray-400 to-slate-600
+          
+          "
+        >
           <input
             id="title"
             type="text"
@@ -38,6 +59,18 @@ function AddSong() {
             value={title}
             onChange={onChange}
             placeholder="Title"
+            className="bg-gray-200
+            border-2 border-gray-300
+            rounded-md
+            md:w-96
+            w-64
+            h-10
+            px-5
+            text-gray-700
+            focus:outline-none
+            focus:border-indigo-500
+            mb-3
+            "
           />
           <input
             id="artist"
@@ -46,6 +79,17 @@ function AddSong() {
             value={artist}
             onChange={onChange}
             placeholder="Artist"
+            className="bg-gray-200
+            border-2 border-gray-300
+            rounded-md
+            md:w-96
+            w-64
+            h-10
+            px-5
+            text-gray-700
+            focus:outline-none
+            focus:border-indigo-500
+            mb-3"
           />
           <input
             id="genre"
@@ -54,6 +98,17 @@ function AddSong() {
             value={genre}
             onChange={onChange}
             placeholder="Genre"
+            className="bg-gray-200
+            border-2 border-gray-300
+            rounded-md
+            md:w-96
+            w-64
+            h-10
+            px-5
+            text-gray-700
+            focus:outline-none
+            focus:border-indigo-500
+            mb-3"
           />
           <input
             id="album"
@@ -62,9 +117,32 @@ function AddSong() {
             value={album}
             onChange={onChange}
             placeholder="Album"
+            className="bg-gray-200
+            border-2 border-gray-300
+            rounded-md
+            md:w-96
+            w-64
+            h-10
+            px-5
+            text-gray-700
+            focus:outline-none
+            focus:border-indigo-500
+            mb-3"
           />
-          <h1 className="text-white">hello</h1>
-          <button type="submit">Add Song</button>
+          <button
+            type="submit"
+            className="
+          border-2 border-gray-300
+          rounded-md
+          md:w-96
+          w-64
+          h-10
+          hover:bg-slate-700
+          hover:text-white
+          "
+          >
+            Add Song
+          </button>
         </form>
       </main>
     </>
