fffe9be04133d768a67a0a455fdfd36d84b6280a
Ibranista <ibranista88@gmail.com>
2023-03-04 02:26:32 +0100

    style the createAccount component.

--- src/auth/CreateUser.tsx
+++ src/auth/CreateUser.tsx
@@ -34,47 +34,55 @@ function CreateUser() {
   return (
     <>
       {/* <FormContainer> */}
-        <form action="">
-          <section>
-            <label htmlFor="displayName">User Name:</label>
-            <input
-              type="text"
-              id="displayName"
-              name="displayName"
-              value={formData.displayName}
-              onChange={handleChange}
-              required
-              placeholder="display name"
-            />
-          </section>
-          <section>
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              onChange={handleChange}
-              value={formData.email}
-              required
-              placeholder="email"
-            />
-          </section>
-          <section>
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              onChange={handleChange}
-              value={formData.password}
-              required
-              placeholder="password"
-            />
-          </section>
-          <button type="submit" onClick={createAccount}>
-            Create Account
-          </button>
-        </form>
+      <form action="" className="mx-auto max-w-md">
+  <section className="mb-4">
+    <label htmlFor="displayName" className="block mb-2">User Name:</label>
+    <input
+      type="text"
+      id="displayName"
+      name="displayName"
+      value={formData.displayName}
+      onChange={handleChange}
+      required
+      placeholder="display name"
+      className="w-full px-3 py-2 border border-gray-300 rounded-md"
+    />
+  </section>
+  <section className="mb-4">
+    <label htmlFor="email" className="block mb-2">Email:</label>
+    <input
+      type="email"
+      id="email"
+      name="email"
+      onChange={handleChange}
+      value={formData.email}
+      required
+      placeholder="email"
+      className="w-full px-3 py-2 border border-gray-300 rounded-md"
+    />
+  </section>
+  <section className="mb-4">
+    <label htmlFor="password" className="block mb-2">Password:</label>
+    <input
+      type="password"
+      id="password"
+      name="password"
+      onChange={handleChange}
+      value={formData.password}
+      required
+      placeholder="password"
+      className="w-full px-3 py-2 border border-gray-300 rounded-md"
+    />
+  </section>
+  <button
+    type="submit"
+    onClick={createAccount}
+    className="mb-3 w-full bg-indigo-500 text-white font-bold py-2 px-4 rounded-md hover:bg-indigo-600"
+  >
+    Create Account
+  </button>
+</form>
+
       {/* </FormContainer> */}
     </>
   );
