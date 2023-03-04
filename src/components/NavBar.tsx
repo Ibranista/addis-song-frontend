c419a2cec39ece5f5a77709d1101f896a6a2d1ea
Ibranista <ibranista88@gmail.com>
2023-03-04 06:43:27 +0100

    implement signout.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,22 @@ import React from "react";
 import { css } from "@emotion/css";
 import { color } from "styled-system";
 import NavBox from "../styles/Box";
+import { auth } from "../auth/firebase";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import { signOut as userSignOut } from "firebase/auth";
 function NavBar() {
+
+   const navigate = useNavigate() 
+  const signOut = async()=>{
+    try{
+      await userSignOut(auth);
+      toast.success("Signed Out");
+      navigate("/AccountCreation");
+    }catch(e:any){
+      toast.error(e.message);
+    }
+  }
   return (
     // <div>
     //   <section
@@ -19,6 +34,15 @@ function NavBar() {
     <>
       <NavBox bg="primary" color="white">
         Explore Options !
+        <button onClick={signOut} className="
+        bg-red-500
+        hover:bg-red-700
+        text-white
+        font-bold
+        py-2
+        px-4
+        rounded-md
+        ">{auth.currentUser?'signOut':''}</button>
       </NavBox>
     </>
   );
